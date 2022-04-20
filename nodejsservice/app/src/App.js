890d911e0e5bb680970024c12d0661af84f1cfad
Carlos Vargas Huaman <carlos.vargas.huaman@avantica.net>
2022-04-20 16:48:38 -0500

    creating some card component

--- nodejsservice/app/src/App.js
+++ nodejsservice/app/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import faker from "@faker-js/faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ui container comments">
+
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Carlos"
+          timeAgo="Today at 6:00PM"
+          comment="Reactero Junior"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Bill"
+          timeAgo="Today at 6:00PM"
+          comment="Reactero Master"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Dracir"
+          timeAgo="Today at 6:00PM"
+          comment="Reactero Dormilon"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
     </div>
   );
 }
