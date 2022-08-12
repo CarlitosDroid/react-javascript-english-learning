b40822d5824c5d6a50eef129f0a777814a8ab199
Carlos Vargas Huaman <cvargas@godaddy.com>
2022-08-12 17:11:22 -0500

    add new project

--- nodejsservice/app/src/App.js
+++ nodejsservice/app/src/App.js
@@ -1,45 +1,47 @@
-import logo from "./logo.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
-import faker from "@faker-js/faker";
-import CommentDetail from "./CommentDetail";
-import ApprovalCard from "./ApprovalCard";
+import { faker } from "@faker-js/faker";
+
+import Header from "./components/Header";
+import AddContact from "./components/AddContact";
+import ContactList from "./components/ContactList";
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = (contact) => {
+    console.log("carlosssss");
+    console.log(faker.datatype.uuid());
+    setContacts([...contacts, { id: faker.datatype.uuid(), ...contact }]);
+  };
+
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+    setContacts(newContactList);
+  };
+
+  useEffect(() => {
+    // READ LOCATION STORAGE
+    // This is for local storage to avoid deleting the data once we refresh the page
+    const retrieveContacts = JSON.parse(
+      localStorage.getItem(LOCAL_STORAGE_KEY)
+    );
+    if (retrieveContacts) setContacts(retrieveContacts);
+  }, []);
+
+  useEffect(() => {
+    // INSERT LOCAL STORAGE
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
-    <div className="ui container comments">
-
-      <ApprovalCard>
-        <div>
-          <h4>Warning!</h4>
-          Are you sure you want to do this?
-        </div>
-      </ApprovalCard>
-      <ApprovalCard>
-        <CommentDetail
-          author="Carlos"
-          timeAgo="Today at 6:00PM"
-          comment="Reactero Junior"
-          avatar={faker.image.image()}
-        />
-      </ApprovalCard>
-
-      <ApprovalCard>
-        <CommentDetail
-          author="Bill"
-          timeAgo="Today at 6:00PM"
-          comment="Reactero Master"
-          avatar={faker.image.image()}
-        />
-      </ApprovalCard>
-
-      <ApprovalCard>
-        <CommentDetail
-          author="Dracir"
-          timeAgo="Today at 6:00PM"
-          comment="Reactero Dormilon"
-          avatar={faker.image.image()}
-        />
-      </ApprovalCard>
+    <div className="ui container">
+      <Header />
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} getContactId={removeContactHandler} />
     </div>
   );
 }
